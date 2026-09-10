0391c7f10d1aa505ebb6d7ce80adfa3dd1f9fdd8
marouanetouzani11 <marouane1games@gmail.com>
2026-09-10 10:33:54 +0100

    jour 02 : terminer le challenge

--- jour-02/day02/challenge/challenge.js
+++ jour-02/day02/challenge/challenge.js
@@ -22,30 +22,27 @@
 
 // Découpe d'abord le problème en petites étapes.
 
+
+let result = 0;
 let codeSecret = 739;
 let essais = 0;
-let trouve = false;
-for (let c1 = 0; c1 <= 9; c1++) {
-  if (trouve === true) {
-    break;
-  }
+
+for (let c1 = 0; c1 <= 9; c1++){
 
   for (let c2 = 0; c2 <= 9; c2++) {
-    if (trouve === true) {
-      break;
-    }
 
-    for (let c3 = 0; c3 <= 9; c3++) {
-      essais++;
+    for (let c3 = 0; c3 <= 9; c3++){
 
-      let codeTeste = (c1 * 100) + (c2 * 10) + c3;
+      let codeTeste = (c1 * 100) + (c2 * 10) + c3
 
-        if (codeTeste === codeSecret) {
+      essais++
+      if (codeTeste === codeSecret) {
         console.log("Code trouvé : " + codeSecret + " après " + essais + " essais");
-
-        trouve = true;
-        break;
+      break
       }
+
     }
+
   }
+
 }
